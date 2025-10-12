9f6c9e45be614f2dcec5bc25ad38cd149eadc5f5
Gagandeep Singh <64951244+gagan-git-sardar@users.noreply.github.com>
2025-10-12 13:34:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,21 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- 1. Burger Menu Logic ---
     const burger = document.querySelector('.burger');
     const navLinks = document.querySelector('.nav-links');
-    const navLinksLi = document.querySelectorAll('.nav-links li');
+    const body = document.querySelector('body');
 
     burger.addEventListener('click', () => {
         // Toggle Nav
         navLinks.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinksLi.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        
+        // Toggle body class to prevent scrolling
+        body.classList.toggle('nav-open');
         // Burger Animation
         burger.classList.toggle('toggle');
     });
